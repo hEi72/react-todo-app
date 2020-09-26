4241e743ee997e30e87ae51350e8c6bce1ed0753
Jeannie Ng <jeang@Jeannies-MacBook-Pro.local>
2020-09-26 17:28:05 +0800

    todo - edit fx

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 import { ListItem, ListItemIcon, ListItemText, Checkbox, ListItemSecondaryAction, IconButton, Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import db from './firebase';
-import firebase from './firebase';
+import firebase from 'firebase';
 
 function Todo(props) {
     // *** Declare var *** //
@@ -31,21 +31,21 @@ function Todo(props) {
         itemTime = `${hh}:${mm}:${ss}`;
     }
 
-    // // *** State & Function - Checkbox *** //
-    // const [checked, setChecked] = React.useState([0]);
+    // *** State & Function - Checkbox *** //
+    const [checked, setChecked] = React.useState([0]);
 
-    // const handleToggle = (value) => () => {
-    //     const currentIndex = checked.indexOf(value);
-    //     const newChecked = [...checked];
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
 
-    //     if (currentIndex === -1) {
-    //         newChecked.push(value);
-    //     } else {
-    //         newChecked.splice(currentIndex, 1);
-    //     }
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
 
-    //     setChecked(newChecked);
-    // };
+        setChecked(newChecked);
+    };
 
     // *** State & Function - Dialog box open & close *** //
     const [open, setOpen] = React.useState(false);
@@ -58,37 +58,43 @@ function Todo(props) {
         setOpen(false);
     };
 
-    // *** Function - Edit item *** //
+    // *** State & Function - Edit item *** //
+    const [update, setUpdate] = useState(''); // display properly while typing
+
     const editItem = (event) => {
+        event.preventDefault(); // stop refresh
+
         db.collection('todos').doc(itemId).update({
-            text: '',
-            timestamp: firebase.firestore.FieldValue.serverTimestamp() // update last modified date
+            text: update,
+            //timestamp: firebase.firestore.FieldValue.serverTimestamp() // update last modified date
         })
     }
     
     // *** Output component *** //
     return (
-        <ListItem key={itemText} role={undefined} dense button 
-        // onClick={handleToggle(itemText)}
-        >
+        <ListItem key={itemText} role={undefined} dense button>
+            <div className="list_icon_text" onClick={handleToggle(itemText)}>
             <ListItemIcon>
             <Checkbox edge="start" tabIndex={-1} disableRipple
-                // checked={checked.indexOf(itemText) !== -1}
+                 checked={checked.indexOf(itemText) !== -1}
                 inputProps={{ 'aria-labelledby': labelId }}
             />
             </ListItemIcon>
 
             <ListItemText id={labelId} primary={itemText} secondary={`${itemDate} ${itemTime}`} />
+            </div>
 
             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" fullWidth={true} maxWidth="sm">
                 <DialogTitle id="form-dialog-title">Edit</DialogTitle>
+                <form>
                 <DialogContent>
-                    <TextField autoFocus margin="dense" id="itemContent" label="New content" fullWidth value={"update"}/>
+                    <TextField autoFocus margin="dense" id="itemContent" label="New content" fullWidth value={update} onChange={event => setUpdate(event.target.value)} />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">Cancel</Button>
-                    <Button onClick={editItem} color="primary">Confirm</Button>
+                    <Button onClick={editItem && handleClose} color="primary" type="submit">Confirm</Button>
                 </DialogActions>
+                </form>
             </Dialog>
 
             <ListItemSecondaryAction>
